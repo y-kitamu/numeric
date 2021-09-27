101db31d5cce1a5d5dd9d5e27f9764ffa91d2a3f
Kitamura <ymyk6602@gmail.com>
2021-09-27 22:02:39 +0900

    add matrix struct

---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+use std::ops::{Index, IndexMut};
+
+pub mod linalg;
+
+struct Matrix<T>
+where
+    T: Clone,
+{
+    rows: usize,
+    cols: usize,
+    data: Vec<T>,
+}
+
+impl<T> Matrix<T>
+where
+    T: Clone,
+{
+    fn new(rows: usize, cols: usize, data: Vec<T>) -> Self {
+        Matrix { rows, cols, data }
+    }
+
+    pub fn rows(&self) -> usize {
+        self.rows
+    }
+
+    pub fn cols(&self) -> usize {
+        self.cols
+    }
+
+    pub fn resize(&mut self, new_rows: usize, new_cols: usize) {
+        self.rows = new_rows;
+        self.cols = new_cols;
+    }
+
+    pub fn assign(&mut self, new_rows: usize, new_cols: usize, val: T) {
+        self.rows = new_rows;
+        self.cols = new_cols;
+        self.data = vec![val; self.rows * self.cols];
+    }
+}
+
+impl<T> Index<usize> for Matrix<T>
+where
+    T: Clone,
+{
+    type Output = [T];
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data[index * self.cols..(index + 1) * self.cols]
+    }
+}
+
+impl<T> IndexMut<usize> for Matrix<T>
+where
+    T: Clone,
+{
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.data[index * self.cols..(index + 1) * self.cols]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matrix() {
+        let rows = 3;
+        let cols = 4;
+        #[rustfmt::skip]
+        let data = vec![
+            1, 2, 3, 4,
+            5, 6, 7, 8,
+            9, 10, 11, 12,
+        ];
+        let mut mat = Matrix::new(rows, cols, data);
+        assert_eq!(mat.rows(), rows);
+        assert_eq!(mat.cols(), cols);
+        assert_eq!(mat[0][0], 1);
+        assert_eq!(mat[1].len(), cols);
+        assert_eq!(mat[2][3], 12);
+
+        mat[2][3] = 8;
+        assert_eq!(mat[2][3], 8);
+        assert_eq!(mat[2][2], 11);
+
+        mat.resize(10, 11);
+        assert_eq!(mat.rows(), 10);
+        assert_eq!(mat.cols(), 11);
+
+        mat.assign(5, 4, 1);
+        assert_eq!(mat.rows(), 5);
+        assert_eq!(mat.cols(), 4);
+        assert_eq!(mat[0][0], 1);
+        assert_eq!(mat[4][3], 1);
+    }
+}
